1870062330522cd79b796d0fdefbdcd19d3e922b
AlanMandriaza <alan.mandriaza@gmail.com>
2023-03-30 19:51:32 +0000

    terminado

--- .learn/exercises/03.3-your-first-route/solution.hide.py
+++ .learn/exercises/03.3-your-first-route/solution.hide.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/todos', methods=['GET'])
 def hello_world():
     return "<h1>Hello!</h1>"
+
 
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
